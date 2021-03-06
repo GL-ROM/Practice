520ad3c334ddb1ee8b9e279efd4e39b709cbb64f
GL-ROM <gabriel.l.romero@gmail.com>
2021-03-05 22:35:49 -0500

    Test

---
+++ jsPractice/loopPractice.js
@@ -0,0 +1,19 @@
+let s = "javascriptloops";
+
+function vowelsAndConsonants(s) {
+    const vowels = 'aeiou';
+    var consonants = '';
+    
+    for(var i = 0; i < s.length; i++) {
+       if (vowels.includes(s[i])) {
+           console.log(s[i]);
+       }
+       else {
+           consonants += s[i] + '\n';
+       }
+    }
+    
+    console.log(consonants.trim());
+}
+
+vowelsAndConsonants(s);
